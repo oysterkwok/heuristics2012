26fce75950fc26877685842a7d2a326ade49d60c
oysterkwok <oyster.hal@gmail.com>
2014-09-05 22:11:13 -0700

    eee

--- Vornoi/final.cpp
+++ Vornoi/final.cpp
@@ -22,38 +22,27 @@ std::pair<int, int> string_test(std::string inp_str) {
 	return std::pair<int, int>(x, y);
 }
 
-//pair<int,int> getNextMove();
-
 int main(int argc, char* argv[]) {
-	//ofstream fout("client.log");
 	if (argc < 2) {
 		puts("Usage: ./a.out TEAM_NAME");
 		return 1;
 	}
+	
 	char tmpstr[MAXTMPARRLEN];
+	
 	setvbuf(stdin, NULL, _IONBF, 0); 
 	setvbuf(stdout, NULL, _IONBF, 0);
-
+	
 	printf("%s\n", argv[1]);
-
+	
 	vector<pair<int,int> > newTokens;
 	// initialize the gameboard
 	// and compute intrinsic score of each square
 	board b1(50, 0.3, 2);
-//	theBoard.ComputeIntrinsicScores(0.3);
-
-//	while (1) {
-	int i = 0;
-		while (i < 7 && fgets(tmpstr, MAXTMPARRLEN, stdin) != NULL) {
-			i ++;
-			//fout << "I got [" << tmpstr << "]\n";
-			//fout.flush();
-			//sscanf(tmpstr, "%d,%d", &x, &y);
-			if (!tmpstr)
-				break;
+	
+	while (1) {
+		if (fgets(tmpstr, MAXTMPARRLEN, stdin) != NULL) {
 			std::pair<int, int> nextMove = string_test(tmpstr);
-			//fout << "I parsed " << nextMove.first << "," << nextMove.second << "\n";
-			//fout.flush();
 			newTokens.clear();
 			if (nextMove.first >= 0)
 				newTokens.push_back(nextMove);
@@ -65,49 +54,8 @@ int main(int argc, char* argv[]) {
 			nextMove = b1.getNextMove1();
 			newTokens.push_back(nextMove);
 			printf("%d,%d\n", nextMove.first, nextMove.second);
-			//fout << " I move " << nextMove.first << "," << nextMove.second << "\n";
-			//fout.flush();
-		} 
-		//else {
-		//	break;
-		//}
-	//}
+		}
+	}
+	
 	return 0;
 }
-
-/*
-pair<int,int> getNextMove(board b, int ratio) {
-	// for each square
-		// place a token there
-		// color the board
-		// get the new score
-
-	// return the move that yields the highest score
-}
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
